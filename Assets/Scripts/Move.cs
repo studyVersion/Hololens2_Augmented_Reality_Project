7627744b810a202ce3d5eb169f3cea5cd4598cae
studyVersion <0314sbadre@e-itaca.es>
2022-12-20 14:14:44 +0100

    Version moddified, errores fixed, build changed to the hololens platform, and a button added to close door with hand press created as a test

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,13 +7,13 @@ public class Move : MonoBehaviour
     public Transform doorPosition;
     public Transform targetPosition;
     private Vector3 referencia;
-    public float speed =2.2f;
+    public float speed =1f;
     public AudioSource doorAudio;
 
     public GameObject luzInterior;
     public GameObject luzExterior;
-
-    public ParticleSystem dustDoor;
+    bool click = false;
+  
 
     bool opening;
     bool closing;
@@ -30,26 +30,33 @@ public class Move : MonoBehaviour
         
          luzInterior.SetActive(true);
          luzExterior.SetActive(true);
-         dustDoor.Stop();
+      
 
     }
 
-
+    public void clickMe(){
+        if(click == false){
+            click =true;
+        }else{
+            click = false;
+        }
+    }
 
     void Update()
     {
-        
+        if(click){
         // Moves the object forward at two units per second.
-        if (Input.GetKey(KeyCode.LeftArrow) && !closing )
+        // if (Input.GetKey(KeyCode.LeftArrow) && !closing )
+        if (!closing )
         {   
             opening= true;
             doorAudio.Play();
         }
 
-        if (Input.GetKey(KeyCode.RightArrow) && !opening ){
-            closing =true;
-            doorAudio.Play();
-        }
+        // if (Input.GetKey(KeyCode.RightArrow) && !opening ){
+        //     closing =true;
+        //     doorAudio.Play();
+        // }
        
         if(opening){      
 
@@ -68,7 +75,12 @@ public class Move : MonoBehaviour
                }            
             
         }
-      
+        }
+        if(!click){
+       if (!opening ){
+            closing =true;
+            doorAudio.Play();
+        }
         if(closing){
              
              move(doorPosition.position, referencia);
@@ -78,11 +90,9 @@ public class Move : MonoBehaviour
 
                 luzInterior.SetActive(true);
                 luzExterior.SetActive(true);
-                dustDoor.Play();
+              
                  //LeftC.transform.Rotate(0,40,0);
-          
-              
-              
+ 
            }
             
         }
@@ -99,6 +109,7 @@ public class Move : MonoBehaviour
         //     doorAudio.Stop();
         // }
 
+    }
     }
     void reset(){
          opening= false;
---
+++ Assets/Scripts/fg.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fg : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshCollider[] colliders = FindObjectsOfType<MeshCollider>();
+int len = colliders.Length;
+for( int i = 0; i < len; i++ ) GameObject.Destroy( colliders[i] );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
