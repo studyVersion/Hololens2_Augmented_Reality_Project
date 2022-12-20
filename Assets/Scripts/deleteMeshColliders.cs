bb6745936ea9c42c1c5feaebfdc1735c17e8e024
studyVersion <0314sbadre@e-itaca.es>
2022-12-20 19:28:56 +0100

    the rest of the errors fixed

--- Assets/Scripts/deleteMeshColliders.cs
+++ Assets/Scripts/deleteMeshColliders.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fg : MonoBehaviour
+public class deleteMeshColliders : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         MeshCollider[] colliders = FindObjectsOfType<MeshCollider>();
-int len = colliders.Length;
-for( int i = 0; i < len; i++ ) GameObject.Destroy( colliders[i] );
+        int len = colliders.Length;
+        for( int i = 0; i < len; i++ ) GameObject.Destroy( colliders[i] );
     }
 
     // Update is called once per frame
