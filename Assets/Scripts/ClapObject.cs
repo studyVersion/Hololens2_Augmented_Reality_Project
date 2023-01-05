f46fe7ceb6ad4674587ff61c629cbc7d1dbfd91a
studyVersion <0314sbadre@e-itaca.es>
2023-01-05 20:10:12 +0100

    Opbject to cut can now be manipulated and come back to its spot

--- Assets/Scripts/ClapObject.cs
+++ Assets/Scripts/ClapObject.cs
@@ -4,37 +4,18 @@ public class ClapObject : MonoBehaviour
 {
     // The object that will be clapped onto
     public GameObject clapTarget;
-
-    // The position and rotation of the clap object relative to the clap target
-    public Vector3 clapPosition;
-    public  Transform tr;
-    public Quaternion clapRotation;
-
-    private void OnTriggerStay(Collider other)
+    public GameObject firstFather;
+    public  Transform targetPosition;
+ 
+    private void OnTriggerEnter(Collider other)
     {
         // Check if the other object is the clap target
         if (other.gameObject == clapTarget)
-        {
-            // Clap the object onto the target by setting the clap target as its parent
-           // transform.SetParent(clapTarget.transform, true);
-
-            // Set the position and rotation of the clap object relative to the clap target
-            transform.position = tr.position;
-           // transform.localRotation = clapRotation;
+        
+            transform.position = targetPosition.position;
+            transform.rotation = targetPosition.rotation;
+           
+           
         }
-    }
-/*
-    private void OnTriggerExit(Collider other)
-    {
-        // Check if the other object is the clap target
-        if (other.gameObject == clapTarget)
-        {
-            // Un-clap the object by setting its parent to null
-            //transform.SetParent(null, true);
-
-            // Set the position and rotation of the clap object back to their original values
-            transform.localPosition = Vector3.zero;
-            transform.localRotation = Quaternion.identity;
-        }
-    }*/
+  
 }
